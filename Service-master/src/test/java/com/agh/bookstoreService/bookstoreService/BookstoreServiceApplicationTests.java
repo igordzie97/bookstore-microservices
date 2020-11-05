3503de4e4d213395cf2e2a781d56239bc656d47e
igordzie97 <34041060+igordzie97@users.noreply.github.com>
2020-11-05 11:02:14 +0100

    docker-compose v2

---
+++ Service-master/src/test/java/com/agh/bookstoreService/bookstoreService/BookstoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.bookstoreService.bookstoreService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
